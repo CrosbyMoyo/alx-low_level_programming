3ee0fa3c3bb3cfa269c9a084244d4c63f40ffa4f
CrosbyMoyo <blessingbmoyo@gmail.com>
2021-06-28 22:00:19 +0200

    added all my project files

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,28 +1,59 @@
+/*
+ * File: 101-keygen.c
+ * Auth: Brennan D Baraban
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 
 /**
-* main - Entry point
-* Return: return 0
-*/
+ * main - Generates random valid passwords for the
+ *        program 101-crackme.
+ *
+ * Return: Always 0.
+ */
 int main(void)
- {
-	 int contador;
-	 int  checksum;
-	 int  aleatorio;
+{
+	char password[84];
+	int index = 0, sum = 0, diff_half1, diff_half2;
 
-	srand(time(NULL));
-	checksum = 2772;
-	contador = 0;
+	srand(time(0));
 
-	while (checksum > 122)
+	while (sum < 2772)
 	{
-		aleatorio = (rand() % 100);
-		printf("%c", aleatorio);
-		checksum -= aleatorio;
-		contador++;
+		password[index] = 33 + rand() % 94;
+		sum += password[index++];
 	}
-	printf("%c", checksum);
-        return 0;
- }
+
+	password[index] = '\0';
+
+	if (sum != 2772)
+	{
+		diff_half1 = (sum - 2772) / 2;
+		diff_half2 = (sum - 2772) / 2;
+		if ((sum - 2772) % 2 != 0)
+			diff_half1++;
+
+		for (index = 0; password[index]; index++)
+		{
+			if (password[index] >= (33 + diff_half1))
+			{
+				password[index] -= diff_half1;
+				break;
+			}
+		}
+		for (index = 0; password[index]; index++)
+		{
+			if (password[index] >= (33 + diff_half2))
+			{
+				password[index] -= diff_half2;
+				break;
+			}
+		}
+	}
+
+	printf("%s", password);
+
+	return (0);
+}
